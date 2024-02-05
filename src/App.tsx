297338bdf555c187b9611fab29b5869d019b2665
DevGauravBawa <gauravbawa0609@gmail.com>
2024-02-05 21:12:28 +0530

    [refactor] modified the globals css and tailwind config file [feat] add route and layout outlet for private and public route using react router dom package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
-import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import AuthLayout from "./_auth/AuthLayout"
+import SigninForm from "./_auth/forms/SigninForm"
+import SignupForm from "./_auth/forms/SignupForm"
+import RootLayout from "./_root/RootLayout"
+import { Home } from "./_root/pages"
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* public route */}
+        <Route element={<AuthLayout />}>
+          <Route
+            path="/sign-in"
+            element={<SigninForm />}
+          />
+          <Route
+            path="/sign-up"
+            element={<SignupForm />}
+          />
+        </Route>
+        {/* private route */}
+        <Route element={<RootLayout />}>
+          <Route
+            index
+            element={<Home />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
